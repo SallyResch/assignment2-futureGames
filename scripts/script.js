214c0f8c65249c731fc82dd595f135cb1a8888eb
SallyResch <sallyresch27@gmail.com>
2025-11-06 11:16:32 +0100

    added link to script file and added the begining of the code for guessThe NumberGame

---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+//alert("GUESS THE NUMBER GAME: Guess a number between 1 and 100, you have 10 attempts to success! Good Luck!");
+
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+let guessesArray = [];
