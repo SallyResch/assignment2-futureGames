e370f883cc3e5077143a394fe2ed7720406bb00e
SallyResch <sallyresch27@gmail.com>
2025-11-06 11:55:11 +0100

    added arrowFunctions. One for checking the guess is to low or to high and then reduces the userPoints

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,21 @@
-//alert("GUESS THE NUMBER GAME: Guess a number between 1 and 100, you have 10 attempts to success! Good Luck!");
+alert("GUESS THE NUMBER GAME: \nGuess a number between 1 and 100 \nYou have 10 attempts to success! \nGood Luck! \nOh and by the way:\n You have 10 points, if you get to zero you loose mouhahaha");
 
-let randomNumber = Math.floor(Math.random() * 100) + 1;
-let guessesArray = [];
+const numberGuessingGame = () => {
+  let randomNumber = Math.floor(Math.random() * 100) + 1;
+  //If guesses Array contains more then 10 
+  let guessesArray = [];
+  let userPoints = 10;
+  const userGuess = parseInt(prompt("Enter a number between 1 and 100:"));
+
+  const guessCheck = () => {
+    if (userGuess < 1 || userGuess > 100) {
+      --userPoints
+      console.log(userPoints);
+    }
+  }
+  guessCheck();
+
+}
+//numberGuessingGame();
+
+
