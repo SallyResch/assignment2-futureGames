b3a7820f4a71c9d754b12a0495055a06f2879ddd
SallyResch <sallyresch27@gmail.com>
2025-12-02 15:52:44 +0100

    added html to the project with basic rules and a function to remove the rules when the game starts

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,11 @@
+const startButton = document.querySelector(".start-button");
+const rulesContent = document.querySelector(".rules-content");
+const gameContent = document.querySelector(".game-content");
 
-
-
-
-
-
+const startGame = () => {
+  startButton.addEventListener("click", () => {
+    rulesContent.classList.add("hidden");
+    gameContent.classList.remove("hidden");
+  })
+}
+startGame();
