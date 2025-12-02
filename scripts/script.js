33306bfecdef96a087e63f1bfa6e8930d81de355
SallyResch <sallyresch27@gmail.com>
2025-12-02 18:18:51 +0100

    added inputfield

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,21 @@
 const startButton = document.querySelector(".start-button");
 const rulesContent = document.querySelector(".rules-content");
 const gameContent = document.querySelector(".game-content");
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+const addInputField = () => {
+  const inputField = document.createElement("input");
+  inputField.type = "number";
+  inputField.classList.add("guess-input");
+  gameContent.appendChild(inputField);
+}
 
 const startGame = () => {
   startButton.addEventListener("click", () => {
     rulesContent.classList.add("hidden");
     gameContent.classList.remove("hidden");
+    addInputField();
   })
+
 }
 startGame();
